5f027d57b22a06916f47eaf1b7cb2faa89dc04df
unknown <jakubjaran@gmail.com>
2021-01-10 22:25:06 +0100

    Change routing to named

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -3,12 +3,18 @@ import 'package:flutter/material.dart';
 class CategoryMealsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+
+    final title = routeArgs['title'];
+    final id = routeArgs['id'];
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Some category'),
+        title: Text(title),
       ),
       body: Center(
-        child: Text('It works!'),
+        child: Text(id),
       ),
     );
   }
